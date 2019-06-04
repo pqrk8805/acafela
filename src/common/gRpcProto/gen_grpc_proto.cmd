4ec19f2f801d5a388f79919313108def579c1c2a
Ki Kim <bartkim@gmail.com>
2019-06-04 15:13:22 -0400

    add gRPC proto script for UserProfile

---
+++ src/common/gRpcProto/gen_grpc_proto.cmd
@@ -0,0 +1,27 @@
+@echo off
+chcp 437
+
+set CMD_PROTOC=..\..\..\external\gRPC\bin\protoc.exe
+set INC_PROTO=..\..\..\external\gRPC\include\third_party\protobuf\src
+set GRPC_PLUGIN_CPP=..\..\..\external\gRPC\bin\grpc_cpp_plugin.exe
+
+set PROTO_RPC_FILES=AcafelaRPC.proto
+
+
+%CMD_PROTOC% ^
+        -I%INC_PROTO% ^
+        -I.\ ^
+        --cpp_out=.\ ^
+        --csharp_out=.\ ^
+		%PROTO_RPC_FILES%
+
+%CMD_PROTOC% ^
+        -I%INC_PROTO% ^
+        -I.\ ^
+        --grpc_out=.\ ^
+        --plugin=protoc-gen-grpc=%GRPC_PLUGIN_CPP% ^
+        %PROTO_RPC_FILES%
+
+
+copy *.h ..\..\server\acafela\src\UserProfile\
+copy *.cc ..\..\server\acafela\src\UserProfile\
