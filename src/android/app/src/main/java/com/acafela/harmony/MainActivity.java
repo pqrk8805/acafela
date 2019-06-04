57c1a1b5bed78c1ff364142ec60c2ad66d286479
박민규/선임연구원/HMingyu88.parkA제어신뢰성/QE팀(mingyu88.park@lge.com) <mingyu88.park@lge.com>
2019-06-05 07:49:00 +0900

    Add UDP PingPong Button
    Add Button on Android Sample
    Spacial thanks : google and stack overflow

--- src/android/app/src/main/java/com/acafela/harmony/MainActivity.java
+++ src/android/app/src/main/java/com/acafela/harmony/MainActivity.java
@@ -1,13 +1,72 @@
 package com.acafela.harmony;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
+    private final String SERVERIPAddr = "192.168.1.6" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton_SND(View v)
+    {
+        WifiManager wifiMan = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiMan.getConnectionInfo();
+        int ipAddress = wifiInf.getIpAddress();
+        String ip = String.format("%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
+        Toast.makeText(this, ip, Toast.LENGTH_LONG).show();
+        sendMessage(ip);
+    }
+
+    private void sendMessage(final String message) {
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            String stringData;
+            @Override
+            public void run() {
+                DatagramSocket ds = null;
+                try {
+                    ds = new DatagramSocket();
+                    InetAddress serverAddr = InetAddress.getByName(SERVERIPAddr);
+                    DatagramPacket dp;
+                    dp = new DatagramPacket(message.getBytes(), message.length(), serverAddr, 5000);
+                    ds.send(dp);
+
+                    byte[] lMsg = new byte[1000];
+                    dp = new DatagramPacket(lMsg, lMsg.length);
+                    ds.receive(dp);
+                    stringData = new String(lMsg, 0, dp.getLength());
+                    final TextView rcvText = findViewById(R.id.RCVText);
+                    rcvText.post(new Runnable() {
+                        public void run() {
+                            rcvText.setText("RCV DATA : " + stringData);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (ds != null) {
+                        ds.close();
+                    }
+                }
+            }
+        });
+        thread.start();
     }
 }
