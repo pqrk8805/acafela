79d25ccf9065f93d35f00e005e730826f6edf5b2
박민규/선임연구원/HMingyu88.parkA제어신뢰성/QE팀(mingyu88.park@lge.com) <mingyu88.park@lge.com>
2019-06-06 05:43:23 +0900

    merge project and change prj setting
    sdk 8.1 -> 10.xx
    빌드 터지면 알려주세요 ,,,

--- src/server/acafela/src/main.cpp
+++ src/server/acafela/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <thread>
+#include <vector>
+void pingpongCommunicator_ex();
 
 int main()
 {
+	std::vector<std::thread *> additionalThreadList;
     std::cout << "Hello World!\n"; 
+	additionalThreadList.push_back(new std::thread(pingpongCommunicator_ex));
+	for(auto * th : additionalThreadList)
+		th->join();
+	return 0;
 }
 
