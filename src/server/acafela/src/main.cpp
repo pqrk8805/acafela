76bb03b79f2e1ce22a9a779f4ee2b7b2df4674ef
박민규/선임연구원/HMingyu88.parkA제어신뢰성/QE팀(mingyu88.park@lge.com) <mingyu88.park@lge.com>
2019-06-07 05:42:39 +0900

    Sorry Ki Kim
    i forgot to remove comment.

--- src/server/acafela/src/main.cpp
+++ src/server/acafela/src/main.cpp
@@ -11,15 +11,15 @@ void pingpongCommunicator_init();
 
 int main(int argc, char** argv)
 {
-    //FUNC_LOGI("Acafela Server started");
-	//
-    //UserProfileRpc userProfileRpc;
-    //int err = userProfileRpc.start("10.0.1.3:9000");
-    //if (err) {
-    //    FUNC_LOGE("ERROR(%d): fail to start UserProfileRpc server", err);
-    //    return -1;
-    //}
-	//
+    FUNC_LOGI("Acafela Server started");
+	
+    UserProfileRpc userProfileRpc;
+    int err = userProfileRpc.start("10.0.1.3:9000");
+    if (err) {
+        FUNC_LOGE("ERROR(%d): fail to start UserProfileRpc server", err);
+        return -1;
+    }
+	
 	pingpongCommunicator_init();
 	for(auto * th : additionalThreadList)
 		th->join();
