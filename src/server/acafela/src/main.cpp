e085a40ccee86effb4d1382c6930ff2199dc431f
Ki Kim <bartkim@gmail.com>
2019-06-05 22:59:09 -0400

    [server] err log

--- src/server/acafela/src/main.cpp
+++ src/server/acafela/src/main.cpp
@@ -3,20 +3,23 @@
 #include <vector>
 #include "UserProfileRpc.h"
 #include "Hislog.h"
+
 #define LOG_TAG "MAIN"
-#define BUFLEN 512
+
 std::vector<std::thread *> additionalThreadList; 
 void pingpongCommunicator_init();
+
 int main(int argc, char** argv)
 {
     FUNC_LOGI("Acafela Server started");
+
     UserProfileRpc userProfileRpc;
     int err = userProfileRpc.start("10.0.1.3:9000");
     if (err) {
-        FUNC_LOGE("ERROR(%d): fail to start UserProfileRpc server");
+        FUNC_LOGE("ERROR(%d): fail to start UserProfileRpc server", err);
         return -1;
     }
-    std::cout << "Hello World!\n";
+
 	pingpongCommunicator_init();
 	for(auto * th : additionalThreadList)
 		th->join();
