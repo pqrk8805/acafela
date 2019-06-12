3b0bfcd8b6db327f67bc6583de9a317e10880d4d
Ki Kim <bartkim@gmail.com>
2019-06-11 20:53:18 -0400

    [server] DirectoryService RPC

---
+++ src/server/acafela/src/DirectoryService/DirectoryServiceRpc.cpp
@@ -0,0 +1,70 @@
+#include "DirectoryServiceRpc.h"
+#include <grpcpp/server_builder.h>
+#include "Hislog.h"
+
+#define LOG_TAG "DS_RPC"
+
+
+DirectoryServiceRpc::DirectoryServiceRpc(IDirectoryService& directoryService)
+  : mServer(nullptr),
+    mDirectoryService(directoryService)
+{
+}
+
+DirectoryServiceRpc::~DirectoryServiceRpc()
+{
+    if (mServer) {
+        mServer->Shutdown();
+        delete mServer;
+        mServer = nullptr;
+    }
+
+    if (mWorker.joinable()) {
+        mWorker.join();
+    }
+}
+
+int DirectoryServiceRpc::start(const std::string& addressUri)
+{
+    ::grpc::ServerBuilder builder;
+    builder.AddListeningPort(
+                        addressUri,
+                        ::grpc::InsecureServerCredentials());
+    builder.RegisterService(this);
+    std::unique_ptr<::grpc::Server> server(builder.BuildAndStart());
+    if (server == nullptr) {
+        FUNC_LOGE("ERROR: fail to create server: %s", addressUri.c_str());
+        return -1;
+    }
+
+    FUNC_LOGI("DirectoryService RPC server listen on: %s", addressUri.c_str());
+    std::thread t ( [this]() { this->wait(); } );
+    mWorker.swap(t);
+
+    return 0;
+}
+
+ void DirectoryServiceRpc::wait()
+ {
+     if (mServer) mServer->Wait();
+ }
+
+void DirectoryServiceRpc::shutdown()
+{
+    mServer->Shutdown();
+}
+
+::grpc::Status DirectoryServiceRpc::update(
+                                    ::grpc::ServerContext* context,
+                                    const ::acafela::rpc::DirInfo* request,
+                                    ::acafela::rpc::Error* response)
+{
+    int err = mDirectoryService.update(
+                                    request->phone_number(),
+                                    request->address());
+    response->set_err(err);
+    response->set_message(err ? "Fail" : "OK");
+
+    return grpc::Status::OK;
+}
+
--- src/server/acafela/src/main.cpp
+++ src/server/acafela/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include "DirectoryService.h"
+#include "DirectoryServiceRpc.h"
 #include "UserProfile.h"
 #include "UserProfileRpc.h"
 #include "Hislog.h"
@@ -15,11 +17,21 @@ int main(int argc, char** argv)
 {
     FUNC_LOGI("Acafela Server started");
 
+    int err = 0;
+
     UserProfile userProfile;
     UserProfileRpc userProfileRpc(userProfile);
-    int err = userProfileRpc.start("localhost:9000");
+    err = userProfileRpc.start("10.0.1.2:9000");
     if (err) {
         FUNC_LOGE("ERROR(%d): fail to start UserProfileRpc server", err);
+        return err;
+    }
+
+    DirectoryService directoryService;
+    DirectoryServiceRpc directoryServiceRpc(directoryService);
+    err = directoryServiceRpc.start("10.0.1.2:9100");
+    if (err) {
+        FUNC_LOGE("ERROR(%d): fail to start DirectoryServiceRpc server", err);
         return err;
     }
 	
