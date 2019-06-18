744054bf6a2fd2c149accc5b60e3acd8cd5dd9d7
이영희/책임연구원/SW Platform(연)Web Media팀(youngh.lee@lge.com) <youngh.lee@lge.com>
2019-06-18 17:39:26 -0400

    Video initial from audio

--- src/android/app/src/main/java/com/acafela/harmony/codec/VideoControllerAsync.java
+++ src/android/app/src/main/java/com/acafela/harmony/codec/VideoControllerAsync.java
@@ -1,4 +1,37 @@
 package com.acafela.harmony.codec;
 
+import com.acafela.harmony.codec.VideoCodecAsync.VideoCallback;
+
 public class VideoControllerAsync {
+    private static final String TAG = VideoControllerAsync.class.getName();
+
+    VideoMediaFormat mVideoMediaFormat = new VideoMediaFormat();
+    VideoCodecAsync mVideoEncoder = new VideoCodecAsync(true);
+    VideoCodecAsync mVideoDecoder = new VideoCodecAsync(false);
+
+    public void start() {
+        mVideoEncoder.start(mVideoMediaFormat.getVideoMediaFormat());
+        mVideoDecoder.start(mVideoMediaFormat.getVideoMediaFormat());
+    }
+
+    public void stop() {
+        mVideoEncoder.stop();
+        mVideoDecoder.stop();
+    }
+
+    public void enqueueRawBytes(byte[] rawBytes) {
+        mVideoEncoder.enqueueInputBytes(rawBytes);
+    }
+
+    public void enqueueEncodedBytes(byte[] encodedBytes) {
+        mVideoDecoder.enqueueInputBytes(encodedBytes);
+    }
+
+    public void setEncoderCallback(VideoCallback callback) {
+        mVideoEncoder.setCallback(callback);
+    }
+
+    public void setDecoderCallback(VideoCallback callback) {
+        mVideoDecoder.setCallback(callback);
+    }
 }
--- src/android/app/src/main/java/com/acafela/harmony/codec/VideoMediaFormat.java
+++ src/android/app/src/main/java/com/acafela/harmony/codec/VideoMediaFormat.java
@@ -1,7 +1,32 @@
 package com.acafela.harmony.codec;
 
 import android.media.MediaFormat;
+import android.media.MediaCodecInfo;
 
 public class VideoMediaFormat {
+
+    private static final String TAG = VideoMediaFormat.class.getName();
+
+    public static final String VIDEO_MIME_TYPE = MediaFormat.MIMETYPE_VIDEO_AVC;
+    private static final int VIDEO_FRAME_RATE = 30;               // 30fps
+    private static final int VIDEO_IFRAME_INTERVAL = 0;  // TRADE-OFF
+    private static final int VIDEO_BIT_RATE = 300000;
+
+    public static final int VIDEO_QUEUE_BOUND = 100;
+
+    private MediaFormat mFormat = new MediaFormat();
+
+    public VideoMediaFormat() {
+        mFormat.setString(MediaFormat.KEY_MIME, VIDEO_MIME_TYPE);
+        mFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        mFormat.setInteger(MediaFormat.KEY_FRAME_RATE, VIDEO_FRAME_RATE);
+        mFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, VIDEO_IFRAME_INTERVAL);
+        mFormat.setInteger(MediaFormat.KEY_BIT_RATE, VIDEO_BIT_RATE);
+    }
+
+    public MediaFormat getVideoMediaFormat() {
+        return mFormat;
+    }
+
 
 }
