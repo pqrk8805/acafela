e905a0d29594c5de536d29ee635cb5dc730586b1
Ki Kim <bartkim@gmail.com>
2019-06-24 17:37:28 -0400

    [phone] port config added '9400' for ConCallReserv

--- src/android/app/src/main/java/com/acafela/harmony/Config.java
+++ src/android/app/src/main/java/com/acafela/harmony/Config.java
@@ -7,4 +7,5 @@ public interface Config
     public final int RPC_PORT_USER_PROFILE = 9000;
     public final int RPC_PORT_DIRECTORY_SERVICE = 9100;
     public final int RPC_PORT_CRYPTO_KEY = 9200;
+    public final int RPC_PORT_CONFERENCE_CALL_RESERVE = 9400;
 }
