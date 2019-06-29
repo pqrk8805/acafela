b70e84cec7be8ce8ed7f6d7d1043abdf059a27dd
Ki Kim <bartkim@gmail.com>
2019-06-29 12:18:29 -0400

    [phone] fix: proximity sensor activation

--- src/android/app/src/main/java/com/acafela/harmony/util/ProximityScreenController.java
+++ src/android/app/src/main/java/com/acafela/harmony/util/ProximityScreenController.java
@@ -3,39 +3,56 @@ package com.acafela.harmony.util;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.PowerManager;
+import android.util.Log;
 
 public class ProximityScreenController
 {
     private static final String LOG_TAG = "ProxScr";
 
     private Context mContext;
+    @SuppressLint("InvalidWakeLockTag")
+    PowerManager.WakeLock mWakeLock;
 
     public ProximityScreenController(Context context)
     {
         mContext = context;
+
+        PowerManager pm = (PowerManager)mContext.getSystemService(
+                                                        Context.POWER_SERVICE);
+        @SuppressLint("InvalidWakeLockTag")
+        PowerManager.WakeLock wl = pm.newWakeLock(
+                                PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,
+                                "HarmonyTag");
+        mWakeLock = wl;
     }
 
     public void activate()
     {
+        Log.i(LOG_TAG, "activate()");
+        /*
         PowerManager pm = (PowerManager)mContext.getSystemService(
                                                         Context.POWER_SERVICE);
         @SuppressLint("InvalidWakeLockTag")
         PowerManager.WakeLock wl = pm.newWakeLock(
                                     PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,
                                     "HarmonyTag");
-        if (wl.isHeld() == false)
-            wl.acquire();
+        */
+        if (mWakeLock.isHeld() == false)
+            mWakeLock.acquire();
     }
 
     public void deactivate()
     {
+        Log.i(LOG_TAG, "deactivate()");
+        /*
         PowerManager pm = (PowerManager)mContext.getSystemService(
                                                         Context.POWER_SERVICE);
         @SuppressLint("InvalidWakeLockTag")
         PowerManager.WakeLock wl = pm.newWakeLock(
                                     PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,
                                     "HarmonyTag");
-        if (wl.isHeld())
-            wl.release();
+        */
+        if (mWakeLock.isHeld())
+            mWakeLock.release();
     }
 }
